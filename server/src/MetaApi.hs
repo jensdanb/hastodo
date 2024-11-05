5553ff03d28ddb4257345dcbb658936a585b8ae5
Your Name <you@example.com>
2024-11-05 22:57:54 +0100

    upgraded GHC and imports. Started Todo API

--- server/src/MetaApi.hs
+++ server/src/MetaApi.hs
@@ -1,12 +1,30 @@
-{-# LANGUAGE DataKinds, TypeOperators #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 
 module MetaApi (MetaAPI) where
 
-import Servant
-import Data.Aeson
+import Data.Aeson (ToJSON)
+import GHC.Generics ( Generic )
+import Servant ( JSON, ReqBody, type (:>), Get, Post )
 
-import Models (ServerConnected)
+-- import Models (ServerConnected)
 
 type MetaAPI = "serverConnected" :> Get '[JSON] String
 
 
+type TodoAPI = "new_todo" :> ReqBody '[JSON] Todo :> Post '[JSON] Todo
+
+data Todo = Todo
+    { id :: String
+    , name :: String
+    , completed :: Bool
+    } deriving (Eq, Show, Generic)
+
+instance ToJSON Todo
