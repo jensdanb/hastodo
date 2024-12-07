0758210a9900c076f75fc997042a271d9492622e
Your Name <you@example.com>
2024-12-07 22:23:36 +0100

    Docker multi-stage build, and fixed Docker not working.

--- server/src/Plumbing.hs
+++ server/src/Plumbing.hs
@@ -4,8 +4,11 @@ module Plumbing where
 import Servant (Application)
 import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Handler.Warp (run)
-import Network.Wai.Middleware.Cors (cors, simpleCors, simpleCorsResourcePolicy, CorsResourcePolicy(..), simpleHeaders)
+import Network.Wai.Middleware.Cors (cors, simpleCors, simpleCorsResourcePolicy, CorsResourcePolicy(..), simpleHeaders, Origin)
 import Network.Wai (Middleware)
+
+localAddr :: Origin
+localAddr = "http://localhost:"
 
 runServer :: Middleware -> Application -> Int -> IO ()
 runServer mWare app portNr = run portNr (mWare app)
@@ -14,9 +17,9 @@ runServerSimpleCors :: Application -> Int -> IO ()
 runServerSimpleCors = runServer simpleCors
 
 myCorsPolicy :: Network.Wai.Middleware.Cors.CorsResourcePolicy
-myCorsPolicy = simpleCorsResourcePolicy   { corsOrigins = Just (["http://localhost:5173"],  True)
-                                    , corsMethods = ["OPTIONS", "GET", "PUT", "POST", "DELETE"]
-                                    , corsRequestHeaders = ["Authorization", "Content-Type"] }
+myCorsPolicy = simpleCorsResourcePolicy { corsOrigins = Just (map (localAddr <>) ["5173", "5050"],  True)
+                                        , corsMethods = ["OPTIONS", "GET", "PUT", "POST", "DELETE"]
+                                        , corsRequestHeaders = ["Authorization", "Content-Type"] }
 
 myCors :: Middleware
 myCors = cors $ const (Just myCorsPolicy)
