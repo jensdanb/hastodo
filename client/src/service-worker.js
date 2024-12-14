ba5dea4fd59e3dbd796956dea244ca0c7566bac2
Your Name <you@example.com>
2024-12-14 22:41:16 +0100

    Setup for Progressive Web App

---
+++ client/src/service-worker.js
@@ -0,0 +1,11 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js');
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === "image", 
+    new workbox.strategies.CacheFirst()
+);
+
+workbox.routing.registerRoute(
+    ({request}) => request.destination === "script", 
+    new workbox.strategies.NetworkFirst()
+);
